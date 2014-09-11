16e60b07b6e0919822377d64c7b10c15e6d9fcd3
Shinmera <shinmera@tymoon.eu>
2014-09-11 17:41:26 +0200

    Set icon and shit, idfk

--- backend.lisp
+++ backend.lisp
@@ -40,7 +40,7 @@
      :themes (dm:get 'plaster-themes (db:query :all))
      :types (dm:get 'plaster-types (db:query :all))))
 
-  (admin:define-panel preferences plaster (:lquery (template "admin-preferences.ctml") :icon "" :tooltip "")
+  (admin:define-panel preferences plaster (:lquery (template "admin-preferences.ctml") :icon "fa-wrench" :tooltip "Set your default editor preferences.")
     (let* ((username (user:username (auth:current)))
            (prefs (dm:get-one 'plaster-users (db:query (:= 'user username)))))
 
